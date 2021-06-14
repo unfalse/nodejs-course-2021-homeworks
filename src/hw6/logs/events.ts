2be7afc786de9b6102c5e06c23af7e2fec0da93f
Iskander Gubaidullin <fadeout01@gmail.com>
2021-06-14 19:58:25 +0500

    fixes for previous homework

--- src/hw6/logs/events.ts
+++ src/hw6/logs/events.ts
@@ -1,9 +1,23 @@
 import { winstonLogger } from "./config";
 
-process.on('unhandledRejection', (error: any) => {
-    winstonLogger.error('Unhandled promise rejection! Details: ', error);
-});
+function once(fn, context) { 
+    var result;
+    return function() { 
+        if (fn) {
+            result = fn.apply(context || this, arguments);
+            fn = null;
+        }
+        return result;
+    };
+}
 
-process.on('uncaughtException', (error: any) => {
-    winstonLogger.error(`Uncaught exception! Details: `, error);
-})
+export const setGlobalHandlers = once(() => {
+    process.on('unhandledRejection', (error: any) => {
+        winstonLogger.error('Unhandled promise rejection! Details: ', error);
+    });
+
+    process.on('uncaughtException', (error: any) => {
+        winstonLogger.error(`Uncaught exception! Details: `, error);
+        process.exit(1);
+    });
+}, this);
